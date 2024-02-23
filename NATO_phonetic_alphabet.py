c58c9bfb1a42141ab6855ebc32f1d3d5b8801567
Mayank pandey <mpandeymp0@gmail.com>
2024-02-23 17:04:54 +0530

    pandas

--- NATO_phonetic_alphabet.py
+++ NATO_phonetic_alphabet.py
@@ -1,10 +1,19 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-# print(data)
-phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
-# print(phonetic_dict)
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Only letters are accepted.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
