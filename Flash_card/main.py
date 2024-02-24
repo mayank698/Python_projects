c69a1ae9becc7e548ab7ae3dfd0ae36968264179
Mayank pandey <mpandeymp0@gmail.com>
2024-02-24 14:12:54 +0530

    tkinter

---
+++ Flash_card/main.py
@@ -0,0 +1,8 @@
+from tkinter import *;
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+my_image = PhotoImage(file="/Flash_card/images/card_front.png")
+button = Button(image=my_image, highlightthickness=0)
+button.pack()
+
