3a55d7e6f863da8fe5e0857672edde6bae1caa36
Mayank pandey <mpandeymp0@gmail.com>
2024-02-24 22:37:40 +0530

    smtp

---
+++ Automated_wisher/main.py
@@ -0,0 +1,24 @@
+from datetime import *
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
+
+today = (datetime.now().month, datetime.now().day)
+data = pandas.read_csv("Automated_wisher/birthday.csv")
+
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"Automated_wisher/letter_templates/letter_{
+        random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]",birthday_person["name"])
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL,MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,to_addrs=birthday_person["email"],msg=f"Subject:Happy Birthday\n\n{contents}")
